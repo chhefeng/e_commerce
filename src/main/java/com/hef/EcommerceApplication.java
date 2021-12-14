f8dbc0bdad20b6a796ebca4087b625d9d417c620
hefeng <chhefeng@gmail.com>
2021-12-14 23:58:07 +0100

    change to postgre, configuration

--- src/main/java/com/hef/EcommerceApplication.java
+++ src/main/java/com/hef/EcommerceApplication.java
@@ -2,6 +2,7 @@ package com.hef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -9,5 +10,6 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+
 
 }
